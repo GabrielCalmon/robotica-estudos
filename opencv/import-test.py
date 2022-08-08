bd8adf4d79ba7d4ecc7c81bce76b608414802dd2
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2022-08-08 13:01:55 -0300

    opencv setup

---
+++ opencv/import-test.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as pltb
+
+image = cv.imread("opencv/images/montanhas.jpg", 1)
+image = cv.resize(image, (800, 400))
+
+cv.imshow('my_tab',image)
+cv.waitKey(0)
+cv.destroyAllWindows()
