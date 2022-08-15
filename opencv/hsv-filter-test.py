71f486a8ce9f8a0631a0d9703f4a99bb88e859f5
gabrielcalmon <jgabrielcalmon1@hotmail.com>
2022-08-15 13:12:27 -0300

    add static param HSV filter

--- opencv/hsv-filter-test.py
+++ opencv/hsv-filter-test.py
@@ -1,8 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('opencv/images/blue-cell-case.jpg')
+imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
 h_min = 0
-h_max = 20
-s_min = 100
+h_max = 255
+s_min = 0
 s_max = 255
-v_min = 100
+v_min = 0
 v_max = 255
 
 lower = np.array([h_min,s_min,v_min])
@@ -16,5 +22,8 @@ mask = cv2.inRange(imgHSV,lower,upper)
 # cv2_imshow(imgBlur)
 
 #Recorte da região delimitada pela máscara
-imgResult = cv2.bitwise_and(img,img,mask=mask)
-cv2_imshow(imgResult)
+#imgResult = cv2.bitwise_and(img,img,mask=mask)
+#cv2.imshow('tab view', imgResult)
+cv2.imshow('tab view', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- opencv/webcam.py
+++ opencv/webcam.py
@@ -1,6 +1,27 @@
 # import the opencv library
 import cv2
-  
+import numpy as np
+
+# uma função de callback vazia
+def nothing(x):
+    pass
+
+def start_trackers():
+    # create trackbars for color change
+    cv2.createTrackbar('HMin','image',0,179,nothing) # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('SMin','image',0,255,nothing)
+    cv2.createTrackbar('VMin','image',0,255,nothing)
+    cv2.createTrackbar('HMax','image',0,179,nothing)
+    cv2.createTrackbar('SMax','image',0,255,nothing)
+    cv2.createTrackbar('VMax','image',0,255,nothing)
+
+    # Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('HMax', 'image', 179)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+
+def get_trackers():
+    pass
   
 # define a video capture object
 vid = cv2.VideoCapture(0)
@@ -8,11 +29,29 @@ vid = cv2.VideoCapture(0)
 while(True):
     # Capture the video frame by frame
     ret, frame = vid.read()
-  
+    imgHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Range mínimo e máximo da cor amarela (HSV)
+    h_min = 104
+    h_max = 110
+    s_min = 150
+    s_max = 199
+    v_min = 127
+    v_max = 184
+
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+
+    # Criação de máscara com base nos limites estabelecidos
+    mask = cv2.inRange(imgHSV,lower,upper)
+    #cv2_imshow(mask)
+
+    #Recorte da região delimitada pela máscara
+    imgResult = cv2.bitwise_and(frame,frame,mask=mask)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', imgResult)
     
-    
+
 
     # the 'q' button is set as the
     # quitting button you may use any
